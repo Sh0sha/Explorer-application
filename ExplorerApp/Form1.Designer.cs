ba37088b24c1b80692aa49a3d60e72dcc0de4e77
siber <siber@SHOSHA>
2023-05-19 20:24:16 +0500

    Добавьте файлы проекта.

---
+++ ExplorerApp/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ExplorerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(760, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(113, 71);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(760, 456);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Back";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(889, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 98);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(889, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(215, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Find";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(889, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 53);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Move";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(889, 94);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(215, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 543);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ ExplorerApp/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ExplorerApp
+{
+    public partial class Form1 : Form
+    {
+        private string currentDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            Load += Form1_Load;
+            textBox1.KeyDown += textBox1_KeyDown;
+            listView1.MouseDoubleClick += listView1_MouseDoubleClick;
+            button1.Click += button1_Click;
+            button4.Click += button4_Click;
+            button2.Click += button2_Click;
+            button3.Click += button3_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+            UpdateDirectory(currentDirectory);
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string newDirectory = textBox1.Text.Trim();
+                if (Directory.Exists(newDirectory))
+                {
+                    currentDirectory = newDirectory;
+                    UpdateDirectory(currentDirectory);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid directory path.");
+                }
+            }
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string parentDirectory = Directory.GetParent(currentDirectory)?.FullName;
+
+            if (parentDirectory != null)
+            {
+                currentDirectory = parentDirectory;
+                UpdateDirectory(currentDirectory);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedItem = listView1.SelectedItems[0].Text;
+                string newPath = Path.Combine(currentDirectory, selectedItem);
+
+                if (Directory.Exists(newPath))
+                {
+                    currentDirectory = newPath;
+                    UpdateDirectory(currentDirectory);
+                }
+            }
+        }
+
+
+
+        private void UpdateDirectory(string path)
+        {
+            try
+            {
+                currentDirectory = path;
+
+                // Update the address bar
+                textBox1.Text = currentDirectory;
+
+
+                // Clear the list view
+                listView1.Items.Clear();
+
+                // Add directories to the list view
+                string[] directories = Directory.GetDirectories(currentDirectory);
+                foreach (string directory in directories)
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(directory);
+                    ListViewItem item = new ListViewItem(dirInfo.Name);
+                    item.SubItems.Add(dirInfo.CreationTime.ToString());
+                    item.SubItems.Add(dirInfo.LastWriteTime.ToString());
+                    item.SubItems.Add("Folder");
+                    listView1.Items.Add(item);
+                }
+
+                // Add files to the list view
+                string[] files = Directory.GetFiles(currentDirectory);
+                foreach (string file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    ListViewItem item = new ListViewItem(fileInfo.Name);
+                    item.SubItems.Add(fileInfo.CreationTime.ToString());
+                    item.SubItems.Add(fileInfo.LastWriteTime.ToString());
+                    item.SubItems.Add(fileInfo.Length.ToString());
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access denied to the directory.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+       
+
+
+
+        private void button2_Click(object sender, EventArgs e)                  /// DELETE
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedFile = listView1.SelectedItems[0].Text;
+                string filePath = Path.Combine(currentDirectory, selectedFile);
+
+                DialogResult result = MessageBox.Show("Are you sure you want to delete the file?", "Confirm Delete", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        File.Delete(filePath);
+                        UpdateDirectory(currentDirectory);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred while deleting the file: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+
+        private List<string> SearchFiles(string directory, string searchQuery)
+        {
+            List<string> foundFiles = new List<string>();  // Specify the type argument as string
+
+            try
+            {
+                string[] files = Directory.GetFiles(directory, searchQuery, SearchOption.AllDirectories);
+                foundFiles.AddRange(files);
+            }
+            catch (Exception)
+            {
+                // Ignore any errors and continue the search
+            }
+
+            return foundFiles; ;
+        }
+ private void button4_Click(object sender, EventArgs e)                 // MOVE
+        {
+
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedFile = listView1.SelectedItems[0].Text;
+                string sourcePath = Path.Combine(currentDirectory, selectedFile);
+
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string destinationPath = Path.Combine(folderBrowserDialog.SelectedPath, selectedFile);
+
+                    try
+                    {
+                        File.Move(sourcePath, destinationPath);
+                        UpdateDirectory(currentDirectory);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred while moving the file: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+         private void button3_Click(object sender, EventArgs e)                             // FIND
+        {
+            string searchQuery = textBox3.Text.Trim();
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                List<string> foundFiles = SearchFiles(currentDirectory, searchQuery);
+                if (foundFiles.Count > 0)
+                {
+                    string message = "Found files matching the search criteria:\n\n";
+                    message += string.Join("\n", foundFiles);
+                    MessageBox.Show(message);
+                }
+                else
+                {
+                    MessageBox.Show("No files found matching the search criteria.");
+                }
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
